4d69524fcac77100ea08c119fdff9a6934f8e318
GitHub Action <action@github.com>
2024-06-19 22:33:28 +0800

    Auto Update 2024-06-19 22:33

--- Loon/Script/lodepuly/PuPuMall/PuPuMall_remove_ads.js
+++ Loon/Script/lodepuly/PuPuMall/PuPuMall_remove_ads.js
@@ -1,7 +1,6 @@
 /*
 脚本引用 https://raw.githubusercontent.com/kelv1n1n/script/main/js/pp.js
 */
-
 let url = $request.url;
 let body = $response.body;
 let obj = JSON.parse(body);
@@ -44,7 +43,17 @@ if (url.indexOf(recommendPro) != -1) {
 }
 
 if (url.indexOf(adv) != -1) {
-  obj.data = obj.data.filter(item => ![50,90].includes(item.region_code)); 
+  obj.data = obj.data.filter(item => ![50,90,320,100].includes(item.region_code)); 
+
+  obj.data = obj.data.map(item => {  
+  if (item.region_code === 2) {  
+    // 过滤掉positions数组中component_code等于860和60的元素  
+    item.positions = item.positions.filter(position => ![890, 60].includes(position.component_code));  
+  }  
+    // 返回最终的元素
+    return item;  
+  });
+  
   body = JSON.stringify(obj);
   $done({body});
 }
