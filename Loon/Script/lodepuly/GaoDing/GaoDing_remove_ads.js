b7e865a03f2921f0dad891df9896bfffe4992296
GitHub Action <action@github.com>
2024-07-06 22:32:51 +0800

    Auto Update 2024-07-06 22:32

--- Loon/Script/lodepuly/GaoDing/GaoDing_remove_ads.js
+++ Loon/Script/lodepuly/GaoDing/GaoDing_remove_ads.js
@@ -1,14 +1,28 @@
-// 2024-07-06 20:44:51
-    var json = JSON.parse($response.body);
+// 2024-07-06 22:29:43
+var json = JSON.parse($response.body);
 
-    var indicesToDelete = [0, 1, 2];
-
-    indicesToDelete.forEach(function (index) {
-        if (json.pits[index]) {
-            for (var prop in json.pits[index]) {
-                delete json.pits[index][prop];
+// 检查 URL 路径是否包含 /oc/exhibitions/template/resources
+if ($request.url.includes("/oc/exhibitions/template/resources")) {
+    // 设置 pits[2].delivery_materials 的所有属性值为空字符串
+    if (json.pits && json.pits[0] && json.pits[0].delivery_materials) {
+        json.pits[0].delivery_materials.forEach(function (bannerData) {
+            for (var prop in bannerData) {
+                bannerData[prop] = "";
             }
-        }
-    });
+        });
+    }
+}
 
-    $done({ body: JSON.stringify(json) });
+// 检查 URL 路径是否包含 /oc/exhibitions/app_mine/resources
+if ($request.url.includes("/oc/exhibitions/app_mine/resources")) {
+    // 设置 pits[2].delivery_materials 的所有属性值为空字符串
+    if (json.pits && json.pits[2] && json.pits[2].delivery_materials) {
+        json.pits[2].delivery_materials.forEach(function (bannerData) {
+            for (var prop in bannerData) {
+                bannerData[prop] = "";
+            }
+        });
+    }
+}
+
+$done({ body: JSON.stringify(json) });
