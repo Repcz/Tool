e7dca0f36b15f54d108d170c88a17665d22245c3
GitHub Action <action@github.com>
2024-07-06 11:34:35 +0800

    Auto Update 2024-07-06 11:34

--- Loon/Script/lodepuly/Tieba/Tieba_remove_ads.js
+++ Loon/Script/lodepuly/Tieba/Tieba_remove_ads.js
@@ -1,5 +1,10 @@
-// 2024-07-05 21:19:40
+// 2024-07-06 11:30:17
 var json = JSON.parse($response.body);
+
+// 删除签到内容
+if (json.user.user_growth.task_info[1]) {
+    delete json.user.user_growth.task_info[1];
+}
 
 // 删除金融内容
 if (json.finance_tab) {
@@ -20,6 +25,32 @@ if (json.banner) {
 if (json.duxiaoman_entry) {
     delete json.duxiaoman_entry;
 }
+
+
+// 精简常用功能
+// if (json.custom_grid[4]) {
+//     delete json.custom_grid[4];
+// }
+
+// if (json.custom_grid[5]) {
+//     delete json.custom_grid[5];
+// }
+
+// if (json.custom_grid[6]) {
+//     delete json.custom_grid[6];
+// }
+
+// if (json.custom_grid[10]) {
+//     delete json.custom_grid[10];
+// }
+
+// if (json.custom_grid[11]) {
+//     delete json.custom_grid[11];
+// }
+
+// if (json.custom_grid[12]) {
+//     delete json.custom_grid[12];
+// }
 
 // 删除小程序列表
 if (json.recom_naws_list) {
