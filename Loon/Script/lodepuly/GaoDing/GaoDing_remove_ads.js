c78ee202f033e48592d5c8c96ce487d64aded466
GitHub Action <action@github.com>
2024-07-06 21:32:48 +0800

    Auto Update 2024-07-06 21:32

--- Loon/Script/lodepuly/GaoDing/GaoDing_remove_ads.js
+++ Loon/Script/lodepuly/GaoDing/GaoDing_remove_ads.js
@@ -1,8 +1,7 @@
 // 2024-07-06 20:44:51
-if ($request.url.includes('/exhibitions/template/resources')) {
     var json = JSON.parse($response.body);
 
-    var indicesToDelete = [0, 1];
+    var indicesToDelete = [0, 1, 2];
 
     indicesToDelete.forEach(function (index) {
         if (json.pits[index]) {
@@ -13,13 +12,3 @@ if ($request.url.includes('/exhibitions/template/resources')) {
     });
 
     $done({ body: JSON.stringify(json) });
-} else if ($request.url.includes('/exhibitions/app_mine/resources')) {
-    var json = JSON.parse($response.body);
-
-    if (json.finance_tab) {
-        delete json.finance_tab;
-    }
-    $done({ body: JSON.stringify(json) });
-} else {
-    $done({});
-}
