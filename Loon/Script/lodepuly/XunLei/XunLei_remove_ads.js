767d5d30c12d0ba1f3de96e38b3e17d95a3b586f
GitHub Action <action@github.com>
2024-07-14 21:01:09 +0800

    Auto Update 2024-07-14 21:01

--- Loon/Script/lodepuly/XunLei/XunLei_remove_ads.js
+++ Loon/Script/lodepuly/XunLei/XunLei_remove_ads.js
@@ -1,9 +1,9 @@
-// 2024-07-14 01:49:13
+// 2024-07-14 20:23:36
 var json = JSON.parse($response.body);
 
-// 删除我的云笔记、迅雷浏览器、迅雷TV、金融专区、迅雷快鸟
+// 删除我的云笔记、迅雷浏览器、迅雷TV、金融专区、迅雷快鸟、隐私协议
 if (json.values && json.values.me_config && json.values.me_config.common_service_list) {
-    var commonServiceIndexes = [0, 1, 2, 3, 4];
+    var commonServiceIndexes = [0, 1, 2, 3, 4, 5];
     commonServiceIndexes.forEach(function(index) {
         if (json.values.me_config.common_service_list[index]) {
             delete json.values.me_config.common_service_list[index];
@@ -13,7 +13,7 @@ if (json.values && json.values.me_config && json.values.me_config.common_service
 
 // 删除迅雷小说
 if (json.values && json.values.me_config && json.values.me_config.entrance_list) {
-    var entranceIndexes = [6, 7];
+    var entranceIndexes = [7];
     entranceIndexes.forEach(function(index) {
         if (json.values.me_config.entrance_list[index]) {
             delete json.values.me_config.entrance_list[index];
