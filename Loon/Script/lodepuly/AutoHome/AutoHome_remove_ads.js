6316714a2fe2520992f669f3b8c2e1ec756a9798
GitHub Action <action@github.com>
2024-07-17 07:35:27 +0800

    Auto Update 2024-07-17 07:35

--- Loon/Script/lodepuly/AutoHome/AutoHome_remove_ads.js
+++ Loon/Script/lodepuly/AutoHome/AutoHome_remove_ads.js
@@ -1,16 +1,36 @@
-// 2024-07-17 06:29:32
-let url = $response.url;
+// 2024-07-17 07:32:53
+let url = $request.url;
 let obj = JSON.parse($response.body);
 
 // 删除社区广场 - 广告轮播图
-if (/\/club_v\d+\.\d+\.\d+\/club\/index\/businessv\d+$/.test(url)) {
+if (/\/club_v\d+(?:\.\d+){2}\/club\/index\/businessv\d+/.test(url)) {
     delete obj.result.bannerlist;
 }
 
 // 删除选车 - 直播浮窗
-if (url.includes("/carstreaming/selectcarportal/reclist") || url.includes("/carstreaming/selectcarportal/seriestopwithtagscard")) {
+if (url.includes("/carstreaming/selectcarportal/reclist")) {
     delete obj.result.liveinfo;
-    delete obj.result.toplist[1];
+}
+
+// 删除选车 - 新车报价页面直播内容
+function removeItemsWithKeywordsForLive(data) {
+    if (Array.isArray(data)) {
+        data.forEach((item, index) => {
+            if (item.text && ["直播中"].some(keyword => item.text.includes(keyword))) {
+                data.splice(index, 1);
+            }
+        });
+    } else if (typeof data === 'object') {
+        for (let key in data) {
+            if (data[key] && typeof data[key] === 'object') {
+                removeItemsWithKeywordsForLive(data[key]);
+            }
+        }
+    }
+}
+
+if (url.includes("/carstreaming/selectcarportal/seriestopwithtagscard")) {
+    removeItemsWithKeywordsForLive(obj);
 }
 
 // 删除二手车 - 竖版轮播图
@@ -19,27 +39,29 @@ if (/apic\/v\d+\/gethomepagefeed/.test(url)) {
 }
 
 // 删除我的页面 - 移除添加我的爱车领券
-if (url.includes("/platform/carserver/carcard/mycardv7")) {
+if (/\/platform\/carserver\/carcard\/mycard\d+/.test(url)) {
     delete obj.result.nocartext;
 }
 
 // 遍历关键词删除所属对象
-function removeItemsWithKeywords(data) {
+function removeItemsWithKeywordsForLoans(data) {
     if (Array.isArray(data)) {
-        data = data.filter(item => {
-            return !item.text || !["低息借钱", "分期购车", "车主贷"].some(keyword => item.text.includes(keyword));
+        data.forEach((item, index) => {
+            if (item.text && ["低息借钱", "分期购车", "车主贷"].some(keyword => item.text.includes(keyword))) {
+                data.splice(index, 1);
+            }
         });
     } else if (typeof data === 'object') {
         for (let key in data) {
             if (data[key] && typeof data[key] === 'object') {
-                removeItemsWithKeywords(data[key]);
+                removeItemsWithKeywordsForLoans(data[key]);
             }
         }
     }
 }
 
 if (/\/platform\/carserver\/((usercenter\/getservicecards)|(carcard\/(mycardv\d+|allcard)))/.test(url)) {
-    removeItemsWithKeywords(obj);
+    removeItemsWithKeywordsForLoans(obj);
 }
 
 $done({ body: JSON.stringify(obj) });
