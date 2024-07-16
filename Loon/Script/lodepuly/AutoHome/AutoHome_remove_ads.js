de621775abd8c385716c191e4d93e0205be9d644
GitHub Action <action@github.com>
2024-07-17 06:35:15 +0800

    Auto Update 2024-07-17 06:35

--- Loon/Script/lodepuly/AutoHome/AutoHome_remove_ads.js
+++ Loon/Script/lodepuly/AutoHome/AutoHome_remove_ads.js
@@ -1,24 +1,20 @@
-// 2024-07-17 05:18:54
-let url = $request.url;
+// 2024-07-17 06:29:32
+let url = $response.url;
 let obj = JSON.parse($response.body);
 
 // 删除社区广场 - 广告轮播图
-if (/^\/club_v\d\.\d\.\d\/club\/index\/businessv\d+$/.test(url)) {
+if (/\/club_v\d+\.\d+\.\d+\/club\/index\/businessv\d+$/.test(url)) {
     delete obj.result.bannerlist;
 }
 
 // 删除选车 - 直播浮窗
-if (url.includes("/carstreaming/selectcarportal/reclist")) {
+if (url.includes("/carstreaming/selectcarportal/reclist") || url.includes("/carstreaming/selectcarportal/seriestopwithtagscard")) {
     delete obj.result.liveinfo;
-}
-
-// 删除选车 - 直播浮窗
-if (url.includes("/carstreaming/selectcarportal/seriestopwithtagscard")) {
     delete obj.result.toplist[1];
 }
 
 // 删除二手车 - 竖版轮播图
-if (url.includes("/apic/v3/gethomepagefeed")) {
+if (/apic\/v\d+\/gethomepagefeed/.test(url)) {
     delete obj.result.otherlist[0]; 
 }
 
@@ -42,7 +38,7 @@ function removeItemsWithKeywords(data) {
     }
 }
 
-if (/^https:\/\/(a\.athm\.cn\/)?mobile\.app\.autohome\.com\.cn\/platform\/carserver\/((usercenter\/getservicecards)|(carcard\/(mycardv7|allcard)))/.test(url)) {
+if (/\/platform\/carserver\/((usercenter\/getservicecards)|(carcard\/(mycardv\d+|allcard)))/.test(url)) {
     removeItemsWithKeywords(obj);
 }
 
