c96b6606a0e9fc955370433651513d6808de12b2
GitHub Action <action@github.com>
2024-09-01 11:39:58 +0800

    Auto Update 2024-09-01 11:39

--- Loon/Script/lodepuly/ColorfulClouds/ColorfulClouds_remove_ads.js
+++ Loon/Script/lodepuly/ColorfulClouds/ColorfulClouds_remove_ads.js
@@ -1,7 +1,5 @@
 // 2024-07-26 23:11:46
-
 const url = $request.url;
-let header = $request.headers;
 let obj = JSON.parse($response.body);
 if (url.includes("/api.caiyunapp.com/v1/activity")) {
   if (url.includes("&type_id=A03&")) {
@@ -18,19 +16,14 @@ if (url.includes("/api.caiyunapp.com/v1/activity")) {
     }
   } else {
     // 其他请求
-    obj = {
-      status: "ok",
-      interval: 2592000,
-      id: "1",
-      activities: [
-        {
-          items: [{ text: "", image_light: "", link: "", activity_name: "", id: "1", image_dark: "" }],
-          type: "activity_icon",
-          name: "",
-          carousel: "5000"
-        }
-      ]
-    };
+    obj = { status: "ok", activities: [{ items: [] }] };
+  }
+} else if (url.includes("/api/v1/user_detail")) {
+  if (obj?.vip_info?.svip) {
+    if (obj?.vip_info?.svip) {
+      obj.vip_info.svip.is_auto_renewal = true;
+      obj.vip_info.svip.expires_time = "3742732800";
+    }
   }
 } else if (url.includes("/wrapper.cyapi.cn/v1/activity")) {
   // 彩云推广
@@ -44,19 +37,37 @@ if (url.includes("/api.caiyunapp.com/v1/activity")) {
     }
   } else {
     // 其他请求
-    obj = {
-      status: "ok",
-      interval: 2592000,
-      id: "1",
-      activities: [
-        {
-          items: [{ text: "", image_light: "", link: "", activity_name: "", id: "1", image_dark: "" }],
-          type: "activity_icon",
-          name: "",
-          carousel: "5000"
-        }
-      ]
-    };
+    obj = { status: "ok", activities: [{ items: [] }] };
+  }
+} else if (url.includes("/v1/vip_info")) {
+  // 我的页面
+  if (obj.vip) {
+    obj.vip.expires_time = "4030000000";
+  }
+  if (obj.svip) {
+    obj.svip.expires_time = "4030000000";
+  }
+} else if (url.includes("/v2/user")) {
+  // 我的页面
+  if (obj?.result) {
+    obj.result.svip_given = 730;
+    obj.result.is_phone_verified = true;
+    obj.result.is_xy_vip = true;
+    obj.result.vip_expired_at = 4030000000.16;
+    obj.result.is_vip = true;
+    obj.result.xy_svip_expire = 4030000000.16;
+    if (obj?.result?.wt) {
+      if (obj.result.wt.vip) {
+        obj.result.wt.vip.enabled = true;
+        obj.result.wt.vip.expired_at = 4030000000.16;
+        obj.result.wt.vip.svip_expired_at = 4030000000.16;
+      }
+      obj.result.wt.svip_given = 730;
+    }
+    obj.result.is_primary = true;
+    obj.result.xy_vip_expire = 4030000000.16;
+    obj.result.svip_expired_at = 4030000000.16;
+    obj.result.vip_type = "s";
   }
 }
 $done({ body: JSON.stringify(obj) });
