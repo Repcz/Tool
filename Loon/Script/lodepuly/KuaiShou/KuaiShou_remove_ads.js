02461c1333f9ae9451a446bcd0bf15e856b48883
GitHub Action <action@github.com>
2024-09-06 00:41:06 +0800

    Auto Update 2024-09-06 00:41

--- Loon/Script/lodepuly/KuaiShou/KuaiShou_remove_ads.js
+++ Loon/Script/lodepuly/KuaiShou/KuaiShou_remove_ads.js
@@ -3,7 +3,7 @@ const url = $request.url;
 let obj = JSON.parse($response.body);
 
 // 检查URL是否包含特定路径
-if (url.includes("/rest/n/feed/selection?")) {
+if (url.includes("/rest/n/feed/selectionFast?")) {
     // 遍历feeds.ad路径
     if (obj.feeds && Array.isArray(obj.feeds)) {
         obj.feeds = obj.feeds.filter(feed => {
