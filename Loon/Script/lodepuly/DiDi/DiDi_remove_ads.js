f5cbad02fb3c3a844c35d7535e80aa1af95ceb2f
GitHub Action <action@github.com>
2024-09-05 14:41:18 +0800

    Auto Update 2024-09-05 14:41

--- Loon/Script/lodepuly/DiDi/DiDi_remove_ads.js
+++ Loon/Script/lodepuly/DiDi/DiDi_remove_ads.js
@@ -1,8 +1,7 @@
 /*
 脚本引用 https://raw.githubusercontent.com/ZenmoFeiShi/Qx/main/Didichuxing.js
 */
-// 2024-07-24 13:57
-
+// 2024-08-20 09:49
 const url = $request.url;
 if (!$response.body) $done({});
 
@@ -22,23 +21,27 @@ if (url.includes("/other/pGetSceneList")) {
 }
 
 if (url.includes("/homepage/v1/core")) {
-  // 保留打车、代驾、青桔骑行
-  const keepNavIds = ['dache_anycar', 'driverservice', 'bike' ];
+  const keepNavIds = ['dache_anycar', 'driverservice', 'bike'];
   if (obj.data && obj.data.order_cards && obj.data.order_cards.nav_list_card && obj.data.order_cards.nav_list_card.data) {
     obj.data.order_cards.nav_list_card.data = obj.data.order_cards.nav_list_card.data.filter(item => keepNavIds.includes(item.nav_id));
   }
-  // 保留底部tap首页、我的
-  const keepBottomNavIds = ['v6x_home', 'home_page' , 'user_center' ];
+  const keepBottomNavIds = ['v6x_home', 'home_page', 'user_center'];
   if (obj.data && obj.data.disorder_cards && obj.data.disorder_cards.bottom_nav_list && obj.data.disorder_cards.bottom_nav_list.data) {
     obj.data.disorder_cards.bottom_nav_list.data = obj.data.disorder_cards.bottom_nav_list.data.filter(item => keepBottomNavIds.includes(item.id));
   }
 }
 
 if (url.includes("/ota/na/yuantu/infoList")) {
-if (obj.data && obj.data.disorder_cards && obj.data.disorder_cards.top_banner_card && obj.data.disorder_cards.top_banner_card.data && obj.data.disorder_cards.top_banner_card.data[0] && obj.data.disorder_cards.top_banner_card.data[0].T === "yuentu_top_banner") {
-    // 移除顶部卡片
+  if (obj.data && obj.data.disorder_cards && obj.data.disorder_cards.top_banner_card && obj.data.disorder_cards.top_banner_card.data && obj.data.disorder_cards.top_banner_card.data[0] && obj.data.disorder_cards.top_banner_card.data[0].T === "yuentu_top_banner") {
     obj.data.disorder_cards.top_banner_card.data.splice(0, 1);
- }
+  }
+}
+
+if (url.includes("/gulfstream/passenger-center/v2/other/pInTripLayout")) {
+  const namesToRemove = ["passenger_common_casper"];
+  obj.data.order_components = obj.data.order_components.filter(
+    component => !(component.name && namesToRemove.includes(component.name))
+  );
 }
 
 if (url.includes("/usercenter/me")) {
