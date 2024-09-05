9f4862b76ec92455b0deccd86e28cae43177d5aa
GitHub Action <action@github.com>
2024-09-05 11:36:33 +0800

    Auto Update 2024-09-05 11:36

--- Loon/Script/lodepuly/ColorfulClouds/ColorfulClouds_remove_ads.js
+++ Loon/Script/lodepuly/ColorfulClouds/ColorfulClouds_remove_ads.js
@@ -1,4 +1,4 @@
-// 2024-09-01 17:00
+// 2024-09-05 11:06:15
 const url = $request.url;
 let obj = JSON.parse($response.body);
 if (url.includes("/api.caiyunapp.com/v1/activity")) {
@@ -18,13 +18,6 @@ if (url.includes("/api.caiyunapp.com/v1/activity")) {
     // 其他请求
     obj = { status: "ok", activities: [{ items: [] }] };
   }
-} else if (url.includes("/api/v1/user_detail")) {
-  if (obj?.vip_info?.svip) {
-    if (obj?.vip_info?.svip) {
-      obj.vip_info.svip.is_auto_renewal = true;
-      obj.vip_info.svip.expires_time = "3742732800";
-    }
-  }
 } else if (url.includes("/wrapper.cyapi.cn/v1/activity")) {
   // 彩云推广
   if (["&type_id=A03&"]?.includes(url)) {
@@ -38,36 +31,6 @@ if (url.includes("/api.caiyunapp.com/v1/activity")) {
   } else {
     // 其他请求
     obj = { status: "ok", activities: [{ items: [] }] };
-  }
-} else if (url.includes("/v1/vip_info")) {
-  // 我的页面
-  if (obj.vip) {
-    obj.vip.expires_time = "4030000000";
-  }
-  if (obj.svip) {
-    obj.svip.expires_time = "4030000000";
-  }
-} else if (url.includes("/v2/user")) {
-  // 我的页面
-  if (obj?.result) {
-    obj.result.svip_given = 730;
-    obj.result.is_phone_verified = true;
-    obj.result.is_xy_vip = true;
-    obj.result.vip_expired_at = 4030000000.16;
-    obj.result.is_vip = true;
-    obj.result.xy_svip_expire = 4030000000.16;
-    if (obj?.result?.wt) {
-      if (obj.result.wt.vip) {
-        obj.result.wt.vip.enabled = true;
-        obj.result.wt.vip.expired_at = 4030000000.16;
-        obj.result.wt.vip.svip_expired_at = 4030000000.16;
-      }
-      obj.result.wt.svip_given = 730;
-    }
-    obj.result.is_primary = true;
-    obj.result.xy_vip_expire = 4030000000.16;
-    obj.result.svip_expired_at = 4030000000.16;
-    obj.result.vip_type = "s";
   }
 }
 $done({ body: JSON.stringify(obj) });
