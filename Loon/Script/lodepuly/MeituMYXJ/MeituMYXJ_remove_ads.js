78865c43165858a33ce0e209b9b45d500b116bf5
GitHub Action <action@github.com>
2024-09-07 23:33:17 +0800

    Auto Update 2024-09-07 23:33

--- Loon/Script/lodepuly/MeituMYXJ/MeituMYXJ_remove_ads.js
+++ Loon/Script/lodepuly/MeituMYXJ/MeituMYXJ_remove_ads.js
@@ -1,22 +1,22 @@
-/// 2024-09-07 21:19:26
+// 2024-09-07 23:02:42
 const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
 if (url.includes("/operation/home.json")) {
     // 遍历 func_list
-    if (obj.list && obj.list.func_list) {
-        obj.list.func_list = obj.list.func_list.filter(item => {
-            return item.name !== "借钱" && item.name !== "AI剪辑";
+    if (obj.response.list) {
+      for (let i in obj.response.list) {
+        obj.response.list[i].func_list = obj.response.list[i].func_list.filter(item => {
+          return item.name !== "借钱" && item.name !== "AI剪辑";
         });
+      }
     }
 
     // 遍历 func_bar_type
-    if (obj.list && obj.list.func_bar_type) {
-        obj.list.func_bar_type = obj.list.func_bar_type.filter(item => {
+    if (obj.response.list && obj.response.list.func_bar_type) {
+        obj.response.list.func_bar_type = obj.response.list.func_bar_type.filter(item => {
             return item.func_bar_type !== 8;
         });
     }
 }
-
-$done({ body: JSON.stringify(obj) });
