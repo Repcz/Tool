f06b4b37643cd50ba32507bb2871a32b75b0706e
GitHub Action <action@github.com>
2024-09-09 09:54:49 +0800

    Auto Update 2024-09-09 09:54

--- Loon/Script/lodepuly/PiPiXia/PiPiXia_remove_ads.js
+++ Loon/Script/lodepuly/PiPiXia/PiPiXia_remove_ads.js
@@ -1,42 +1,22 @@
-/*
-脚本引用https://raw.githubusercontent.com/ZenmoFeiShi/Qx/main/PPX.js
-*/
-// 2024-02-02 13:51
+// 2024-09-09 09:43:18
 const url = $request.url;
-const scriptEnvironment = typeof $task != 'undefined' ? 'Surge' : (typeof $loon != 'undefined' ? 'Loon' : (typeof $httpClient != 'undefined' ? 'Qx' : 'Unknown'));
-
-if (!$response.body || scriptEnvironment === 'Unknown') {
-  $done({});
-}
-
 let obj = JSON.parse($response.body);
 
-
-function filterProfileEntrances() {
-  let profileEntrances = obj.data.profile_entrances;
-  let titlesToFilter = ['放心借', '创作中心', '原创特权', '小黑屋', '我的订单'];
-
-  obj.data.profile_entrances = profileEntrances.filter(entry => !titlesToFilter.includes(entry.title));
-  fixPos(obj.data.profile_entrances);
+// 处理 /bds/feed/stream/v2/ 路径
+if (url.includes("/bds/feed/stream/v2/")) {
+    if (obj.data && obj.data.data && Array.isArray(obj.data.data)) {
+        obj.data.data = obj.data.data.filter(item => !item.ad_info);
+    }
 }
 
-function filterChannelModel() {
-  if (obj.data.channel_model) {
-    obj.data.channel_model = obj.data.channel_model.filter(item => ["feed", "image_text"].includes(item.event_name));
-    fixPos(obj.data.channel_model);
-  }
-}
-
-function fixPos(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    arr[i].pos = i + 1;
-  }
-}
-
-  if (url.includes("/bds/user/check_in/")) {
-  filterProfileEntrances();
-} else if (url.includes("/bds/feed/channel_list/")) {
-  filterChannelModel();
+// 处理 /bds/user/check_in/ 路径
+if (url.includes("/bds/user/check_in/")) {
+    if (obj.data && obj.data.profile_entrances && Array.isArray(obj.data.profile_entrances)) {
+        obj.data.profile_entrances = obj.data.profile_entrances.filter(item => item.title !== "放心借" && item.title !== "洋钱罐借款");
+    }
+    if (obj.data && obj.data.pet_interface_message) {
+        delete obj.data.pet_interface_message;
+    }
 }
 
 $done({ body: JSON.stringify(obj) });
