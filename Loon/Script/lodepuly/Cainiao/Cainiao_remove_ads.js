5ee2e8f2faa539b182a10cfa2d221370ecb66ec9
GitHub Action <action@github.com>
2024-09-27 14:43:52 +0800

    Auto Update 2024-09-27 14:43

--- Loon/Script/lodepuly/Cainiao/Cainiao_remove_ads.js
+++ Loon/Script/lodepuly/Cainiao/Cainiao_remove_ads.js
@@ -1,13 +1,13 @@
 /*
-脚本引用https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/cainiao.js
+脚本引用 https://raw.githubusercontent.com/RuCu6/Loon/refs/heads/main/Scripts/cainiao.js
 */
-// 2024-09-04 22:35
+// 2024-09-27 11:50
 
 const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-if (url.includes(".guoguo.nbnetflow.ads.mshow")) {
+if (url.includes("/mtop.cainiao.guoguo.nbnetflow.ads.mshow")) {
   // 首页
   if (obj?.data) {
     const items = [
@@ -40,7 +40,7 @@ if (url.includes(".guoguo.nbnetflow.ads.mshow")) {
       }
     }
   }
-} else if (url.includes(".guoguo.nbnetflow.ads.show")) {
+} else if (url.includes("/mtop.cainiao.guoguo.nbnetflow.ads.show")) {
   // 我的页面
   if (obj?.data?.result?.length > 0) {
     // 29338 寄件会员
@@ -65,12 +65,7 @@ if (url.includes(".guoguo.nbnetflow.ads.mshow")) {
       }
     }
   }
-} else if (url.includes(".nbfriend.message.conversation.list")) {
-  // 消息中心
-  if (obj?.data?.data?.length > 0) {
-    obj.data.data = obj.data.data.filter((i) => i?.conversationId?.includes("logistic_message"));
-  }
-} else if (url.includes(".nbpresentation.pickup.empty.page.get")) {
+} else if (url.includes("/mtop.cainiao.nbpresentation.pickup.empty.page.get")) {
   // 取件页面
   if (obj?.data?.result) {
     let ggContent = obj.data.result.content;
@@ -85,7 +80,7 @@ if (url.includes(".guoguo.nbnetflow.ads.mshow")) {
       );
     }
   }
-} else if (url.includes(".nbpresentation.protocol.homepage.get")) {
+} else if (url.includes("/mtop.cainiao.nbpresentation.protocol.homepage.get")) {
   // 首页
   if (obj?.data?.result?.dataList?.length > 0) {
     let newLists = [];
@@ -137,6 +132,29 @@ if (url.includes(".guoguo.nbnetflow.ads.mshow")) {
       newLists.push(item);
     }
     obj.data.result.dataList = newLists;
+  }
+} else if (url.includes("/mtop.nbfriend.message.conversation.list")) {
+  // 消息中心
+  if (obj?.data?.data?.length > 0) {
+    obj.data.data = obj.data.data.filter((i) => i?.conversationId?.includes("logistic_message"));
+  }
+} else if (url.includes("/mtop.cainiao.app.mine.main")) {
+  // 我的页面
+  if (obj?.data) {
+    const items = [
+      "activity", // 热门活动
+      "asset", // 我的权益
+      "banner", // 底部滚动横图
+      "content"
+      // "header", // 头部信息
+      // "order" // 我的订单
+      // "packageArea", // 包裹导入
+    ];
+    for (let i of items) {
+      if (obj.data?.[i]) {
+        delete obj.data[i];
+      }
+    }
   }
 }
 
